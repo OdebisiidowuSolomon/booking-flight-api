1473b670e57563cd8bf01b9e351dbd522a79347b
unknown <odebisiidowu09@gmail.com>
2022-10-08 14:12:39 +0100

    adding files

--- models/Flight.js
+++ models/Flight.js
@@ -1,1 +1,9 @@
-exports.exampleModel = [];
+exports.FlightModel = [
+    {
+      "id": 1,
+      "title": "flight to lagos",
+      "time": "4pm",
+      "price": 6000,
+      "date": "16-03-2022"
+    }
+  ]
